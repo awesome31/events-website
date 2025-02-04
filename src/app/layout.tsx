e10a0e8d4da1e476aa92fc02647a41bd5709e015
Rohit Tyagi <rohit.tyagi@mosaicwellness.in>
2025-02-04 22:14:23 +0530

    Init commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 const poppins = Poppins({
   variable: "--font-poppins",
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
